475973c7412b290df22ef5269b537d9da6d9ced3
Israel Murat Corrêa <israelmurat@gmail.com>
2022-09-08 13:48:04 -0300

    Criacao do projeto

---
+++ src/main/java/com/muratsystems/enchecarrinho/EncheCarrinhoApplication.java
@@ -0,0 +1,13 @@
+package com.muratsystems.enchecarrinho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncheCarrinhoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncheCarrinhoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/muratsystems/enchecarrinho/EncheCarrinhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muratsystems.enchecarrinho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncheCarrinhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
