d18b4006ae21ac1f50459583878d6e8e66f01624
Israel Murat Corrêa <israelmurat@gmail.com>
2022-09-12 08:23:31 -0300

    Desenvolvimento

--- src/main/java/com/muratsystems/enchecarrinho/api/dto/ProductCartDTO.java
+++ src/main/java/com/muratsystems/enchecarrinho/api/dto/ProductCartDTO.java
@@ -1,30 +1,52 @@
 package com.muratsystems.enchecarrinho.api.dto;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter @Setter
-@ToString @NoArgsConstructor
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
 public class ProductCartDTO {
 
 	private ProductDTO product;
 	private Integer quantity;
-	private BigDecimal discountByType;
-	
-	public ProductCartDTO(ProductDTO product, Integer quantity) {
+	private BigDecimal discountByType = BigDecimal.ZERO;
+	private BigDecimal discountByCoupon = BigDecimal.ZERO;
+
+	public ProductCartDTO(ProductDTO product, Integer quantity, BigDecimal discountByCoupon) {
 		this.product = product;
 		this.quantity = quantity;
+		this.discountByCoupon = discountByCoupon != null ? discountByCoupon : BigDecimal.ZERO;
 	}
-	
+
 	public BigDecimal getTotalValue() {
 		if (product != null) {
-			return product.getUnitaryValue().multiply(new BigDecimal(quantity.toString()));
+			return product.getUnitaryValue().multiply(new BigDecimal(quantity.toString())).setScale(2,
+					RoundingMode.HALF_EVEN);
 		}
 		return BigDecimal.ZERO;
 	}
-	
+
+	public BigDecimal getTotalValueWithDiscount() {
+		if (!discountByCoupon.equals(BigDecimal.ZERO) || !discountByType.equals(BigDecimal.ZERO)) {
+			if (product != null) {
+				BigDecimal percent = new BigDecimal("100");
+				BigDecimal total = product.getUnitaryValue().multiply(new BigDecimal(quantity.toString()));
+				BigDecimal valueDiscountCoupon = product.getUnitaryValue().multiply(discountByCoupon).divide(percent, 2,
+						RoundingMode.HALF_EVEN);
+				BigDecimal valueDiscountType = product.getUnitaryValue().multiply(discountByType).divide(percent, 2,
+						RoundingMode.HALF_EVEN);
+				return total.subtract(valueDiscountCoupon).subtract(valueDiscountType).setScale(2,
+						RoundingMode.HALF_EVEN);
+			}
+		}
+		return getTotalValue();
+	}
+
 }
--- src/main/java/com/muratsystems/enchecarrinho/api/dto/ShoppingCartDTO.java
+++ src/main/java/com/muratsystems/enchecarrinho/api/dto/ShoppingCartDTO.java
@@ -2,38 +2,68 @@ package com.muratsystems.enchecarrinho.api.dto;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter @Setter @ToString
+@Getter
+@Setter
+@ToString
 public class ShoppingCartDTO {
-	
+
 	private List<ProductCartDTO> productsCart = new ArrayList<>();
 	private CouponDTO coupon;
 	private BigDecimal progressiveDiscount;
-	
+
 	public void addProducts(ProductDTO product) {
 		boolean isAddProduct = false;
-		BigDecimal totalValueCart = BigDecimal.ZERO;
+		BigDecimal discountByCoupon = coupon != null ? coupon.getDiscountPercentage() : BigDecimal.ZERO;
 		for (ProductCartDTO productCart : productsCart) {
 			if (!isAddProduct && productCart.getProduct().equals(product)) {
 				productCart.setQuantity(productCart.getQuantity() + 1);
+				productCart.setDiscountByCoupon(discountByCoupon);
 				isAddProduct = true;
 			}
-			totalValueCart = totalValueCart.add(productCart.getTotalValue());
 		}
 		if (!isAddProduct) {
-			ProductCartDTO newProductCart = new ProductCartDTO(product, 1);
+			ProductCartDTO newProductCart = new ProductCartDTO(product, 1, discountByCoupon);
 			productsCart.add(newProductCart);
-			totalValueCart = totalValueCart.add(newProductCart.getTotalValue());
 		}
+		setDiscountByType();
+		setDiscountByTotal();
 	}
 	
-	private void setDiscountByTotal(BigDecimal totalValueCart) {
+	private void setDiscountByType() {
+		Map<String, Integer> mapProduct = new HashMap<>();
+		for (ProductCartDTO productCart : productsCart) {
+			String key = productCart.getProduct().getType();
+			if (mapProduct.containsKey(key)) {
+				mapProduct.put(key, mapProduct.get(key) + productCart.getQuantity());
+			} else {
+				mapProduct.put(key, productCart.getQuantity());
+			}
+		}
+		for (ProductCartDTO productCart : productsCart) {
+			productCart.setDiscountByType(BigDecimal.ZERO);
+			String key = productCart.getProduct().getType();
+			if (mapProduct.containsKey(key)) {
+				if (mapProduct.get(key).compareTo(Integer.valueOf(10)) >= 0) {
+					productCart.setDiscountByType(BigDecimal.TEN);
+				}
+			}
+		}
+	}
+
+	private void setDiscountByTotal() {
 		progressiveDiscount = BigDecimal.ZERO;
+		BigDecimal totalValueCart = productsCart.stream()
+				.map(p -> p.getTotalValueWithDiscount())
+				.reduce(BigDecimal.ZERO, BigDecimal::add);
+
 		if (totalValueCart.compareTo(new BigDecimal("1000")) > 0) {
 			progressiveDiscount = new BigDecimal("5");
 		} else if (totalValueCart.compareTo(new BigDecimal("5000")) > 0) {
